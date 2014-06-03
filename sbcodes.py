8699e83b8b3362cf94f418dff03bf76b2d7c29b1
William Jackson <william@subtlecoolness.com>
2014-06-03 15:19:07 -0500

    Add timestamp to print statements

--- sbcodes.py
+++ sbcodes.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python
 
+import datetime
 import json
 import os
 import requests
 import sys
+
+def log(message):
+    t = datetime.datetime.utcnow().replace(microsecond=0)
+    print(u'{} (UTC) - {}'.format(t, message))
 
 path = os.path.dirname(os.path.realpath(__file__))
 conf_file = u'{}/sbcodes.json'.format(path)
@@ -13,10 +18,8 @@ with open(conf_file, u'r') as f:
 
 c = requests.get(u'http://sbcodez.com/')
 code = c.text.partition(u'<span class="code">')[2].partition(u'</span>')[0]
-print(u'Current code is {}'.format(code))
-
 if code == conf.get(u'last_code'):
-    print(u'I already submitted this code.')
+    log(u'I already submitted the code {}.'.format(code))
     sys.exit()
 
 url = u'http://www.swagbucks.com/'
@@ -26,7 +29,7 @@ data = {u'hdnCmd': u'sb-gimme', u'pcode': code}
 for name, urqm in conf.get(u'urqm').iteritems():
     cookies = {u'__urqm': urqm}
     r = requests.post(url, params=params, data=data, cookies=cookies)
-    print(u'{}: {}'.format(name, r.json()[0]))
+    log(u'{}: {}: {}'.format(name, code, r.json()[0]))
 
 conf[u'last_code'] = code
 
