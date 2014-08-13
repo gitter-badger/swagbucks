c393140e2440b5ae37e980ff91426d11c4f6274f
William Jackson <william@subtlecoolness.com>
2014-08-12 20:27:39 -0500

    Convert to Flask app

--- sbcodes.py
+++ sbcodes.py
@@ -1,37 +1,25 @@
-#!/usr/bin/env python
-
 import datetime
 import json
 import os
 import requests
 import sys
 
-def log(message):
-    t = datetime.datetime.utcnow().replace(microsecond=0)
-    print(u'{} (UTC) - {}'.format(t, message))
+app = flask.Flask(__name__)
+app.config.from_envvar(u'SETTINGS_FILE')
 
-path = os.path.dirname(os.path.realpath(__file__))
-conf_file = u'{}/sbcodes.json'.format(path)
-
-with open(conf_file, u'r') as f:
-    conf = json.load(f)
-
-c = requests.get(u'http://sbcodez.com/')
-code = c.text.partition(u'<span class="code">')[2].partition(u'</span>')[0].strip()
-if code == conf.get(u'last_code'):
-    log(u'I already submitted the code {}.'.format(code))
-    sys.exit()
-
-url = u'http://www.swagbucks.com/'
-params = {u'cmd': u'sb-gimme-jx'}
-data = {u'hdnCmd': u'sb-gimme', u'pcode': code}
-
-for name, urqm in conf.get(u'urqm').iteritems():
-    cookies = {u'__urqm': urqm}
-    r = requests.post(url, params=params, data=data, cookies=cookies)
-    log(u'{}: {}: {}'.format(name, code, r.json()[0]))
-
-conf[u'last_code'] = code
-
-with open(conf_file, u'w') as f:
-    json.dump(conf, f)
+@app.route(u'/ping')
+def ping():
+	c = requests.get(u'http://sbcodez.com/')
+	_, _, code = c.text.partition(u'<span class="code">')
+	code, _, _ = code.partition(u'</span>').strip()
+	if code == app.config.get(u'LAST_CODE'):
+		app.logger.debug(u'I already submitted the code {}.'.format(code))
+		return u''
+	url = u'http://www.swagbucks.com/'
+	params = {u'cmd': u'sb-gimme-jx'}
+	data = {u'hdnCmd': u'sb-gimme', u'pcode': code}
+	for name, urql in app.config.get(u'URQM').iteritems():
+		cookies = {u'__urqm': urqm}
+		r = requests.post(url, params=params, data=data, cookies=cookies)
+		app.logger.info(u'{}: {}: {}'.format(name, code, r.json()[0]))
+	app.config[u'LAST_CODE'] = code
