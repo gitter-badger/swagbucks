54a4f1e6012a790d1ad80699476ac698501284a6
William Jackson <william@subtlecoolness.com>
2015-06-12 23:20:32 -0500

    Convert back to stand-alone script

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn sbcodes:app
--- sbcodes.py
+++ sbcodes.py
@@ -1,27 +1,49 @@
-import flask
+#!/usr/bin/env python3
+
+import datetime
+import json
+import os
+import pathlib
 import requests
 
-app = flask.Flask(__name__)
-app.config.from_envvar(u'SETTINGS_FILE')
 
-@app.route(u'/ping')
-def ping():
-	c = requests.get(u'http://sbcodez.com/')
-	_, _, code = c.text.partition(u'<span class="code">')
-	code, _, _ = code.partition(u'</span>')
-	code = code.strip()
-	if code == app.config.get(u'LAST_CODE'):
-		app.logger.debug(u'I already submitted the code {}.'.format(code))
-		return u''
-	url = u'http://www.swagbucks.com/'
-	params = {u'cmd': u'sb-gimme-jx'}
-	data = {u'hdnCmd': u'sb-gimme', u'pcode': code}
-	for name, urqm in app.config.get(u'URQM').iteritems():
-		cookies = {u'__urqm': urqm}
-		r = requests.post(url, params=params, data=data, cookies=cookies)
-		app.logger.info(u'{}: {}: {}'.format(name, code, r.json()[0]))
-	app.config[u'LAST_CODE'] = code
-	return u'pong'
+def log(message):
+    t = datetime.datetime.utcnow().replace(microsecond=0)
+    print('{} {}'.format(t, message))
 
-if __name__ == u'__main__':
-    app.run(host=u'0.0.0.0', debug=True)
+
+def main():
+    log('Starting up.')
+    conf = {}
+    home = pathlib.Path(os.environ.get('HOME')).resolve()
+    conf_file = home / '.config/sbcodes.json'
+    if not conf_file.parent.exists():
+        conf_file.parent.mkdir(parents=True)
+    if conf_file.exists():
+        with conf_file.open() as f:
+            conf = json.load(f)
+
+    c = requests.get('http://sbcodez.com/')
+    _, _, code = c.text.partition('<span class="code">')
+    code, _, _ = code.partition('</span>')
+    code = code.strip()
+
+    if code == conf.get('last_code'):
+        log('I already submitted the code {!r}.'.format(code))
+    else:
+        url = 'http://www.swagbucks.com/'
+        params = {'cmd': 'sb-gimme-jx'}
+        data = {'hdnCmd': 'sb-gimme', 'pcode': code}
+
+        for name, urqm in conf.get('urqm', {}).items():
+            cookies = {'__urqm': urqm}
+            r = requests.post(url, params=params, data=data, cookies=cookies)
+            log('{}: {}: {}'.format(name, code, r.json()[0]))
+        conf['last_code'] = code
+
+    with conf_file.open('w') as f:
+        json.dump(conf, f, indent=4, sort_keys=True)
+
+
+if __name__ == '__main__':
+    main()
